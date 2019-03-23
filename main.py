f30f669ab747c5eebf738a34f3900c8f37491d51
YMG <16290>
2019-03-23 12:22:53 -0400

    getting elements from mesh file

--- main.py
+++ main.py
@@ -23,19 +23,57 @@ with open(Meshpath, 'r') as mshFile:
     for n in range(Nnodes):
         split = NodesLines[n].split(" ")
         split[-1] = split[-1].strip() # removes the \n in the last element, Node_Z
-        Node_X.append(split[1])
-        Node_Y.append(split[2])
-        Node_Z.append(split[3])
+        Node_X.append(float(split[1]))
+        Node_Y.append(float(split[2]))
+        Node_Z.append(float(split[3]))
     #print ('Size of Nx = ' + str(len(Node_X)))
     #print ('Size of Ny = ' + str(len(Node_Y)))
     #print ('Size of Nz = ' + str(len(Node_Z)))
 
     #print(Node_Z)
 
+    mshFile.readline()
+    mshFile.readline()
+    Nelms = int(mshFile.readline())
+    print('[LOG Mesh] Total Number of Elements: ' + str(Nelms))
+
+    ElmLines = []
+    for n in range(Nelms):
+        ElmLines.append(mshFile.readline())
+
+    # split the lines to get type of element
+    ElmType = []
+
+    for n in range(Nelms):
+        splitElms = ElmLines[n].split(" ")
+        splitElms[-1] = splitElms[-1].strip()  # removes the \n in the last element
+        ElmType.append(int(splitElms[1]))
+
+    #print(ElmType)
+
+    #TO DO: get surfaces!!!
+
+    #get 4node elements
+    Ntets = 0
+    TetsLines = []
+    for n in range(Nelms):
+        #print (ElmType[n])
+        if ElmType[n] == 4:
+            Ntets +=1
+            TetsLines.append(ElmLines[n])
+
+    #TO DO: Get physical element IDs
+
+    print('[LOG Mesh] Number of Tets: ' + str(Ntets))
+
+    # Get Nodes of each tets
+    Tets2Nodes = []
+    for n in range(Ntets):
+        splitTets = TetsLines[n].split(" ")
+        splitTets[-1] = splitTets[-1].strip()  # removes the \n in the last node ID
+        fournodes = [splitTets[5], splitTets[6], splitTets[7], splitTets[8]]
+        Tets2Nodes.append(fournodes)
 
 
-
-
-
-
-#mshFile.close() #prevents leaking over maximum allowed file descriptor needed only when read explicitly
+    #for n in range(Ntets):
+     #   print(Tets2Nodes[n])
