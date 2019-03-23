70469fe3f9c621390703fbfe011b9a84e74f8ee3
YMG <ymosadde@kent.edu>
2019-03-23 07:35:27 -0400

    reading msh file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
 
+Meshpath = "Mesh/sphere_test.msh"
+mshFile = open(Meshpath, 'r')
+mshFileName = mshFile.name
+print('[LOG]    Reading Mesh File: ' + mshFileName)
 
-print('this is main')
-
+print(mshFile.__sizeof__())
