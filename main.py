91080df5e072df960a4d4ab0f7a18d0ff47ae8aa
YMG <ymosadde@kent.edu>
2019-03-23 08:49:45 -0400

    reading mesh using "with" line by line

--- main.py
+++ main.py
@@ -2,9 +2,21 @@
 Meshpath = "Mesh/sphere_test.msh"
 with open(Meshpath, 'r') as mshFile:
     mshFileName = mshFile.name
+    print('[LOG Mesh] File Name: ' + mshFileName)
+    mshFile.readline()
+    MeshFormat = mshFile.readline()
+    print('[LOG Mesh] Format: ' + MeshFormat)
+    mshFile.readline()
+    mshFile.readline()
+    Nnodes = int(mshFile.readline())
+    print('[LOG Mesh] Nodes =  ' + str(Nnodes))
 
-print('[LOG]    Reading Mesh File: ' + mshFileName)
+    Nodeslist = []
 
-print(mshFile.__sizeof__())
+    for n in range(Nnodes):
+        Nodeslist.append(mshFile.readline())
+    print(Nodeslist[1])
+
+
 
 #mshFile.close() #prevents leaking over maximum allowed file descriptor needed only when read explicitly
