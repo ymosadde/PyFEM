ef3ce7189134bd4d119126d492cd8c7af1da1e16
YMG <ymosadde@kent.edu>
2019-03-24 08:28:04 -0400

    created Gmsh class/moved all gmsh operations

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import getMSH
+import get_mesh_info
 
-Mesh_File = "Mesh/sphere_test.msh"
+gmsh_file = "Mesh/sphere_test.msh"
 
 
-tet_to_nodes = []
-getMSH.getmesh(Mesh_File, tet_to_nodes)
 
-print(tet_to_nodes)
+
+gmsh = get_mesh_info.Gmsh(gmsh_file)
+get_mesh_info.get_mesh_info(gmsh)
+
+print(gmsh.Name)
+print(gmsh.Version)
+print(gmsh.Nnodes)
+print(gmsh.Nelms)
+print(gmsh.Ntets)
+#print(gmsh.Tets2Nodes[10])
