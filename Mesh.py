88e21ee63e65a148deb274e2826e2485612a6fb0
YMG <ymosadde@kent.edu>
2019-03-31 01:42:06 -0400

    create mesh class

--- Mesh.py
+++ Mesh.py
@@ -1,43 +1,53 @@
+import numpy as np
 class Mesh:
 
-    def __init__(self, tets2nodes):
-        print('Making Initial Mesh from Gmsh:')
+    def __init__(self, tets2nodes, current_nodes_pos):
 
-        self.nodes_pos = []
-        self.tets2nodes = tets2nodes
-        
-        #self.my_tet_pos = []
-        #self.my_tet_vol = []
-
-    def update_nodes_pos(self, current_nodes_pos):
         self.nodes_pos = current_nodes_pos
-        
-    '''
-    def get_tets_pos(self):
+        self.tets2nodes = tets2nodes
+
+        self.tet_pos = []
+        self.tet_vol = []
+
+    def update(self):
         nodes_pos = self.nodes_pos
-        print(nodes_pos)
         tets2nodes = self.tets2nodes
         Ntets = len(tets2nodes)
+        Nnodes = len(nodes_pos)
 
-        com_x = []
-        com_y = []
-        com_z = []
+        tet_pos = []
+        volume = []
+
         for t in range(Ntets):
             n1 = int(tets2nodes[t][0])
             n2 = int(tets2nodes[t][1])
             n3 = int(tets2nodes[t][2])
             n4 = int(tets2nodes[t][3])
 
-            nx_avg = (self.nod_x[n1 - 1] + self.nod_x[n2 - 1] + self.nod_x[n3 - 1] + self.nod_x[n4 - 1]) / 4
-            ny_avg = (self.nod_y[n1 - 1] + self.nod_y[n2 - 1] + self.nod_y[n3 - 1] + self.nod_y[n4 - 1]) / 4
-            nz_avg = (self.nod_z[n1 - 1] + self.nod_z[n2 - 1] + self.nod_z[n3 - 1] + self.nod_z[n4 - 1]) / 4
-            com_x.append(nx_avg)
-            com_y.append(ny_avg)
-            com_z.append(nz_avg)
+            # MAKE ARRAY OUT OF LISTS
+            n1_arr = np.asarray(nodes_pos[n1 - 1])
+            n2_arr = np.asarray(nodes_pos[n2 - 1])
+            n3_arr = np.asarray(nodes_pos[n3 - 1])
+            n4_arr = np.asarray(nodes_pos[n4 - 1])
 
-        tet_pos = com_x, com_y, com_z
-        return tet_pos
-'''
+            nx_avg = (n1_arr[0] + n2_arr[0] + n3_arr[0] + n4_arr[0])/4
+            ny_avg = (n1_arr[1] + n2_arr[1] + n3_arr[1] + n4_arr[1])/4
+            nz_avg = (n1_arr[2] + n2_arr[2] + n3_arr[2] + n4_arr[2])/4
 
-    def update(self):
-        pass
+            # UPDATE TET POSITION (COM)
+            tet_pos.append([nx_avg, ny_avg, nz_avg])
+
+            # UPDATE TET VOLUME
+            vec12 = n2_arr - n1_arr  # vec12 = n2 - n1
+            vec13 = n3_arr - n1_arr
+            vec14 = n4_arr - n1_arr
+
+            AB_crossv_AC = np.cross(vec12, vec13)
+            AD_dot_AB_crossv_AC = np.dot(vec14, AB_crossv_AC)
+            volume.append(AD_dot_AB_crossv_AC / 6)
+
+        self.tet_pos = tet_pos
+        self.tet_vol = volume
+
+
+
--- main.py
+++ main.py
@@ -8,25 +8,7 @@ gmsh = Gmsh.Gmsh(file)
 gmsh.get_gmesh_info()
 gmsh.log()
 
-mesh = Mesh.Mesh(gmsh.tets2nodes)
-mesh.update_nodes_pos(gmsh.nodes_pos)
+mesh = Mesh.Mesh(gmsh.tets2nodes, gmsh.nodes_pos)
+mesh.update()
 
 
-mesh.my_tet_pos[10]
-'''
-tet = Tet.Tet(gmsh)
-tet.get_tet_pos()
-tet.get_tet_vol()
-
-v = 0
-for t in range(gmsh.Ntets):
-    v += tet.Volume[t]
-#print('total volume ' + str(v))
-r = 1
-v_theory = 4 / float(3) * 3.14 * r ** 3
-#print('theory volume ' + str(v_theory))
-# mesh = Mesh.Mesh()
-
-print(tet.get_tet_pos())
-#Gmsh.happy()
-'''
